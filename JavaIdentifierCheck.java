e4fb56652fcdbe8c5dfc5794c5257ffe80c9fe23
Eric <ericyoung111@gmail.com>
2022-06-04 08:10:40 -0600

    made the method that checks for spaces

--- JavaIdentifierCheck.java
+++ JavaIdentifierCheck.java
@@ -1,17 +1,34 @@
-
 import java.util.Scanner;
 public class JavaIdentifierCheck {
     public static void main(String[] args) {
+        // Create instance of scanner
         Scanner keyboard = new Scanner(System.in);
+        // Initialize userInput variable
         String userInput = " ";
-        System.out.println("Enter the name of your Java variable.");
-        userInput = keyboard.nextLine();
-        System.out.println(FirstLetter(userInput));
+        while(!userInput.equalsIgnoreCase("Q")) {
+            // Ask user to enter variable name
+            System.out.println("Enter the name of your Java variable. Enter Q to quit.");
+            userInput = keyboard.nextLine();
+            if(userInput.equalsIgnoreCase("Q")) {
+                break;
+            }
+            System.out.println("First character is letter: " + FirstLetter(userInput));
+            System.out.println("Variable contains spaces: " + CheckContainsSpaces(userInput));
+        }
 
         keyboard.close();
     }
 
     static Boolean FirstLetter(String userInput) {
         return Character.isLetter(userInput.charAt(0));
+    }
+
+    static Boolean CheckContainsSpaces(String userInput) {
+        for(int i = 0; i < userInput.length(); i++) {
+            if(userInput.charAt(i) == ' ') {
+                return true;
+            }
+        }
+        return false;
     }
 }
