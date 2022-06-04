8bb938f24a476deaaf7ccbf243c9df97802f5059
Eric <ericyoung111@gmail.com>
2022-06-04 10:38:07 -0600

    made the only letters and digits method

--- JavaIdentifierCheck.java
+++ JavaIdentifierCheck.java
@@ -12,10 +12,19 @@ public class JavaIdentifierCheck {
             if(userInput.equalsIgnoreCase("Q")) {
                 break;
             }
-            System.out.println("First character is letter: " + FirstLetter(userInput));
-            System.out.println("Variable contains spaces: " + CheckContainsSpaces(userInput));
+            if(!FirstLetter(userInput)) {
+                System.out.println("The first character is not a letter.");
+            }
+            if(CheckContainsSpaces(userInput)) {
+                System.out.println("The variable contains spaces. It is illegal");
+            }
+            if(!OnlyLettersDigits(userInput)) {
+                System.out.println("The variable contains characters that are neither a letter or a digit. This is poor style");
+            }
+            if(FirstLetter(userInput) && !CheckContainsSpaces(userInput) && OnlyLettersDigits(userInput)) {
+                System.out.println("The submitted variable begins with a letter, does not contain spaces and only consists of letters and digits, therefore it is good!");
+            }
         }
-
         keyboard.close();
     }
 
@@ -30,5 +39,15 @@ public class JavaIdentifierCheck {
             }
         }
         return false;
+    }
+
+    static Boolean OnlyLettersDigits(String userInput) {
+        for (int i = 0; i < userInput.length(); i++) {
+            char ch = userInput.charAt(i);
+            if(!Character.isDigit(ch) && !Character.isLetter(ch)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
